d50d9a6cd465461587460bc8d5768ef7d147901b
nandakishore <nandakishore695@gmail.com>
2025-03-01 11:24:04 +0530

    first

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns:[
+      {
+        protocol: "https",
+        hostname: "images.pexels.com", // Add unsplash.com to the allowed domains
+      }
+    ]
+  },
+  //  output: "export",
 };
-
 export default nextConfig;
